5b7f538c96b6b3d3a8b2296be1ea2e7d7b6f6801
Omer Somech <117981892+omersomech@users.noreply.github.com>
2024-02-18 11:32:36 +0200

    init

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginBox } from "@/components/component/login-box";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <LoginBox />
+    </div>
+  );
+};
+
+export default Page;
